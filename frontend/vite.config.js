977115cb3468c5b2ec77ce0a88212a03b1ded5fa
Exodia <deluca.davidee@gmail.com>
2026-04-26 16:49:44 +0200

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/static',
+    outDir: path.resolve(__dirname, '../backend/static'),
     emptyOutDir: true,
   },
   server: {
